c9b2389bc8e9accc48c76097e25236b4880e8ccc
Mascl <info@clawsundesign.com>
2025-06-05 17:52:59 +0200

    change events fetch to performances

--- api/group-events.js
+++ api/group-events.js
@@ -21,7 +21,7 @@ module.exports = async (req, res) => {
 
     const token = tokenRes.data.access_token;
 
-    const eventsRes = await axios.get(`https://api.competitionsuite.com/v3/groups/${groupId}/events`, {
+    const eventsRes = await axios.get(`https://api.competitionsuite.com/v3/groups/${groupId}/performances`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
