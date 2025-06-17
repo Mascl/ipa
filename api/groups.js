dd0e3dc90c525d5e1dc42149a89c1691d9cd3665
Mascl <info@clawsundesign.com>
2025-06-17 12:40:11 -0500

    group groups by division name

--- api/groups.js
+++ api/groups.js
@@ -4,15 +4,18 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 module.exports = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
 
   try {
-    const tokenRes = await axios.post("https://api.competitionsuite.com/v3/oauth2/token", new URLSearchParams({
-      grant_type: "client_credentials",
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-    }));
+    const tokenRes = await axios.post(
+      "https://api.competitionsuite.com/v3/oauth2/token",
+      new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+      })
+    );
 
     const token = tokenRes.data.access_token;
 
@@ -22,9 +25,17 @@ module.exports = async (req, res) => {
       },
     });
 
-    console.log("Fetched groups:", groupsRes.data);
+    const groups = groupsRes.data.data;
 
-    res.status(200).json(groupsRes.data);
+    // Group by division.name
+    const grouped = {};
+    for (const group of groups) {
+      const divName = group.division?.name || "Uncategorized";
+      if (!grouped[divName]) grouped[divName] = [];
+      grouped[divName].push(group);
+    }
+
+    res.status(200).json({ grouped });
   } catch (err) {
     console.error("Error fetching groups:", {
       message: err.message,
